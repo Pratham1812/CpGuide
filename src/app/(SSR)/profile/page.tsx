468d571ac5d220fdd721f32bc4155a5cbe38674c
unknown <agarwalpratham1812@gmail.com>
2023-07-14 20:50:44 +0530

    Fix:merge conflicts

--- src/app/(SSR)/profile/page.tsx
+++ src/app/(SSR)/profile/page.tsx
@@ -1,12 +1,11 @@
-"use client"
-import { useEffect, useState } from 'react';
-import { Card, Button, Spinner, Container, Collapse } from 'react-bootstrap';
-import { useCookies } from 'react-cookie';
-import  Accordion  from '../../../components/Accordion';
+"use client";
+import { useEffect, useState } from "react";
+import { Card, Button, Spinner, Container, Collapse } from "react-bootstrap";
+import { useCookies } from "react-cookie";
+import Accordion from "../../../components/Accordion";
 
-
-import { toast } from 'react-hot-toast';
-import { FaAngleDown, FaAngleRight } from 'react-icons/fa';
+import { toast } from "react-hot-toast";
+import { FaAngleDown, FaAngleRight } from "react-icons/fa";
 
 interface UserData {
   fname: string;
@@ -16,7 +15,7 @@ interface UserData {
 }
 
 const ProfilePage = () => {
-  const [cookies] = useCookies(['token']);
+  const [cookies] = useCookies(["token"]);
   const [username, setUserName] = useState<string | null>(null);
   const [email, setUserEmail] = useState<string | null>(null);
   const [fname, setUserFname] = useState<string | null>(null);
@@ -36,11 +35,11 @@ const ProfilePage = () => {
 
   useEffect(() => {
     const fetchProfileData = async () => {
-      const endpoint = 'http://127.0.0.1:8000/api/auth/profile';
+      const endpoint = "http://127.0.0.1:8000/api/auth/profile";
       const options = {
-        method: 'GET',
+        method: "GET",
         headers: {
-          Authorization: 'Bearer ' + cookies['token'],
+          Authorization: "Bearer " + cookies["token"],
         },
       };
 
@@ -55,11 +54,11 @@ const ProfilePage = () => {
           setUserLname(obj.lname);
           setUserFname(obj.fname);
         } else {
-          toast.error('Error fetching profile data');
+          toast.error("Error fetching profile data");
         }
       } catch (error) {
-        console.error('Error fetching profile data:', error);
-        toast.error('Error fetching profile data');
+        console.error("Error fetching profile data:", error);
+        toast.error("Error fetching profile data");
         setUserName(null);
       } finally {
         setLoading(false);
@@ -71,7 +70,7 @@ const ProfilePage = () => {
 
   const accordionItems = [
     {
-      title: 'Item #1',
+      title: "Item #1",
       content: (
         <div>
           <strong>This is the first item's accordion body.</strong> It is hidden
@@ -81,32 +80,29 @@ const ProfilePage = () => {
       ),
     },
     {
-      title: 'Item #2',
+      title: "Item #2",
       content: (
         <div>
           <strong>This is the second item's accordion body.</strong> It is
           hidden by default, but shown when the title is clicked. It will also
           be hidden if the title is clicked again or when another item is
-          clicked. 
+          clicked.
         </div>
       ),
     },
     {
-      title: 'Item #3',
+      title: "Item #3",
       content: (
         <div>
           <strong>This is the third item's accordion body.</strong> It is hidden
           by default, but shown when the title is clicked. It will also be
           hidden if the title is clicked again or when another item is clicked.
-          
         </div>
       ),
     },
   ];
 
-
   return (
-    
     <Card className="m-4 p-4 bg-light border border-primary rounded">
       <Card.Body>
         <Card.Title className="mb-4 text-primary fw-bold display-4">
@@ -120,128 +116,116 @@ const ProfilePage = () => {
             </Spinner>
           </div>
         ) : (
-
           <Container>
             {username && (
               <Card className="border border-primary p-4 mb-4">
-          <div>
-          <Container className='mb-4'>
-            {username ? (
-              <Card className="border border-primary p-4">
+                <div>
+                  <Container className="mb-4">
+                    {username ? (
+                      <Card className="border border-primary p-4">
+                        <Card.Title className="text-primary fw-bold h5 mb-4">
+                          Profile Information
+                        </Card.Title>
+                        <ul className="list-unstyled">
+                          <li>
+                            <strong className="text-primary">
+                              First Name:
+                            </strong>{" "}
+                            {fname}
+                          </li>
+                          <li>
+                            <strong className="text-primary">Last Name:</strong>{" "}
+                            {lname}
+                          </li>
+                          <li>
+                            <strong className="text-primary">Email:</strong>{" "}
+                            {email}
+                          </li>
+                        </ul>
+                      </Card>
+                    ) : (
+                      <p className="text-danger">No profile data available.</p>
+                    )}
 
-                <Card.Title className="text-primary fw-bold h5 mb-4">
-                  Profile Information
-                </Card.Title>
-                <ul className="list-unstyled">
-                  <li>
-                    <strong className="text-primary">First Name:</strong>{' '}
-                    {fname}
-                  </li>
-                  <li>
-                    <strong className="text-primary">Last Name:</strong>{' '}
-                    {lname}
-                  </li>
-                  <li>
-                    <strong className="text-primary">Email:</strong> {email}
-                  </li>
-                </ul>
-              </Card>
-            ) : (
-              <p className="text-danger">No profile data available.</p>
-            )}
+                    <Card className="border border-primary p-4">
+                      <Card.Title className="text-primary fw-bold h5 mb-4">
+                        Collapsible Widgets
+                      </Card.Title>
+                      <Button
+                        variant="link"
+                        onClick={toggleOuterCollapse}
+                        aria-expanded={outerOpen}
+                        aria-controls="outer-collapse-content"
+                        className={`d-flex align-items-center mt-3 ${
+                          outerOpen ? "rotate" : ""
+                        }`}
+                      >
+                        <span className={`me-2 ${outerOpen ? "rotate" : ""}`}>
+                          {outerOpen ? <FaAngleDown /> : <FaAngleRight />}
+                        </span>
+                        <strong>Toggle Outer Widget</strong>
+                      </Button>
+                      <Collapse in={outerOpen}>
+                        <div id="outer-collapse-content">
+                          <hr className="my-4" />
+                          <Card className="border border-primary p-4">
+                            <Card.Title className="text-primary fw-bold h5 mb-4">
+                              Inner Widget
+                            </Card.Title>
+                            <Button
+                              variant="link"
+                              onClick={toggleInnerCollapse}
+                              aria-expanded={innerOpen}
+                              aria-controls="inner-collapse-content"
+                              className={`d-flex align-items-center mt-3 ${
+                                innerOpen ? "rotate" : ""
+                              }`}
+                            >
+                              <span
+                                className={`me-2 ${innerOpen ? "rotate" : ""}`}
+                              >
+                                {innerOpen ? <FaAngleDown /> : <FaAngleRight />}
+                              </span>
+                              <strong>Toggle Inner Widget</strong>
+                            </Button>
+                            <Collapse in={innerOpen}>
+                              <div id="inner-collapse-content">
+                                <Card.Body>
+                                  <p>
+                                    This is the inner widget content. It can be
+                                    expanded or collapsed based on the button
+                                    click.
+                                  </p>
+                                </Card.Body>
+                              </div>
+                            </Collapse>
+                          </Card>
+                        </div>
+                      </Collapse>
+                    </Card>
+                  </Container>
 
-            <Card className="border border-primary p-4">
-              <Card.Title className="text-primary fw-bold h5 mb-4">
-                Collapsible Widgets
-              </Card.Title>
-              <Button
-                variant="link"
-                onClick={toggleOuterCollapse}
-                aria-expanded={outerOpen}
-                aria-controls="outer-collapse-content"
-                className={`d-flex align-items-center mt-3 ${
-                  outerOpen ? 'rotate' : ''
-                }`}
-              >
-                <span className={`me-2 ${outerOpen ? 'rotate' : ''}`}>
-                  {outerOpen ? (
-                    <FaAngleDown />
-                  ) : (
-                    <FaAngleRight />
-                  )}
-                </span>
-                <strong>Toggle Outer Widget</strong>
-              </Button>
-              <Collapse in={outerOpen}>
-                <div id="outer-collapse-content">
-                  <hr className="my-4" />
-                  <Card className="border border-primary p-4">
-                    <Card.Title className="text-primary fw-bold h5 mb-4">
-                      Inner Widget
-                    </Card.Title>
-                    <Button
-                      variant="link"
-                      onClick={toggleInnerCollapse}
-                      aria-expanded={innerOpen}
-                      aria-controls="inner-collapse-content"
-                      className={`d-flex align-items-center mt-3 ${
-                        innerOpen ? 'rotate' : ''
-                      }`}
-                    >
-                      <span className={`me-2 ${innerOpen ? 'rotate' : ''}`}>
-                        {innerOpen ? (
-                          <FaAngleDown />
-                        ) : (
-                          <FaAngleRight />
-                        )}
-                      </span>
-                      <strong>Toggle Inner Widget</strong>
-                    </Button>
-                    <Collapse in={innerOpen}>
-                      <div id="inner-collapse-content">
-                        <Card.Body>
-                          <p>
-                            This is the inner widget content. It can be
-                            expanded or collapsed based on the button click.
-                          </p>
-                        </Card.Body>
-                      </div>
-                    </Collapse>
-                  </Card>
+                  <Container>
+                    {username ? (
+                      <Card className="border border-primary p-4">
+                        <Card.Title className="text-primary fw-bold h5 mb-4">
+                          Coding Status
+                        </Card.Title>
+                        <ul className="list-unstyled">
+                          <Accordion items={accordionItems} />
+                        </ul>
+                      </Card>
+                    ) : (
+                      <p className="text-danger">No profile data available.</p>
+                    )}
+                  </Container>
                 </div>
-              </Collapse>
-            </Card>
-          </Container>
-          
-          
-          <Container>
-            {username ? (
-              <Card className="border border-primary p-4">
-                <Card.Title className="text-primary fw-bold h5 mb-4">
-                  Coding Status
-                </Card.Title>
-                <ul className="list-unstyled">
-                <Accordion items={accordionItems} />
-                </ul>
-
               </Card>
-              
-
-
-            ) : (
-              <p className="text-danger">No profile data available.</p>
             )}
           </Container>
-          </div>
-          
         )}
       </Card.Body>
     </Card>
-    
-    
-
-   
-    
   );
 };
 
