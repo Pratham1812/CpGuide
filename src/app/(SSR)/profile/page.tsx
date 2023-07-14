7b5ef78e99f4700853b937dca1d087cc1658b265
RecoZz <omraturi1@gmail.com>
2023-07-14 15:56:54 +0530

    primary collapsible added

--- src/app/(SSR)/profile/page.tsx
+++ src/app/(SSR)/profile/page.tsx
@@ -2,7 +2,9 @@
 import { useEffect, useState } from 'react';
 import { Card, Button, Spinner, Container } from 'react-bootstrap';
 import { useCookies } from 'react-cookie';
-import { Accordion } from 'react-bootstrap';
+import  Accordion  from '../../../components/Accordion';
+
+
 import { toast } from 'react-hot-toast';
 
 interface UserData {
@@ -18,6 +20,7 @@ const ProfilePage = () => {
   const [email, setUserEmail] = useState(null);
   const [fname, setUserFname] = useState(null);
   const [lname, setUserLname] = useState(null);
+  
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
@@ -30,16 +33,20 @@ const ProfilePage = () => {
         Authorization: 'Bearer ' + cookies['token'],
       }
     }
-
+      try{
       const response = await fetch(endpoint, options);
       const data = await response.json();
       const obj = JSON.parse(data)
-   
+     
       setUserName(obj.username)
       setUserEmail(obj.email)
       setUserLname(obj.lname)
       setUserFname(obj.fname)
       setLoading(false)
+      }catch{
+        console.log("err")
+        setUserName(null)
+      }
       }
   fetchdata()
  
@@ -69,30 +76,55 @@ const ProfilePage = () => {
   //     setLoading(false);
   //   }
   // };
-  const [visible, setVisible] = useState(false)
+  const accordionItems = [
+    {
+      title: 'Item #1',
+      content: (
+        <div>
+          <strong>This is the first item's accordion body.</strong> It is hidden
+          by default, but shown when the title is clicked. It will also be
+          hidden if the title is clicked again or when another item is clicked.
+        </div>
+      ),
+    },
+    {
+      title: 'Item #2',
+      content: (
+        <div>
+          <strong>This is the second item's accordion body.</strong> It is
+          hidden by default, but shown when the title is clicked. It will also
+          be hidden if the title is clicked again or when another item is
+          clicked. 
+        </div>
+      ),
+    },
+    {
+      title: 'Item #3',
+      content: (
+        <div>
+          <strong>This is the third item's accordion body.</strong> It is hidden
+          by default, but shown when the title is clicked. It will also be
+          hidden if the title is clicked again or when another item is clicked.
+          
+        </div>
+      ),
+    },
+  ];
+
+
+
+
+
+
 
   return (
-    <div>
+    
     <Card className="m-4 p-4 bg-light border border-primary rounded">
       <Card.Body>
         <Card.Title className="mb-4 text-primary fw-bold display-4">
           User Profile
         </Card.Title>
-        {/* <Button
-          onClick={fetchProfileData}
-          variant="primary"
-          className="mb-3"
-          disabled={loading}
-        ></Button> */}
-          {/* {loading ? (
-            <>
-              <Spinner animation="border" size="sm" className="me-2" />
-              Loading...
-            </>
-          ) : (
-            'Fetch Profile Data'
-          )} */}
-        
+            
 
         {loading ? (
           <div className="d-flex justify-content-center my-4">
@@ -101,7 +133,8 @@ const ProfilePage = () => {
             </Spinner>
           </div>
         ) : (
-          <Container>
+          <div>
+          <Container className='mb-4'>
             {username ? (
               <Card className="border border-primary p-4">
                 <Card.Title className="text-primary fw-bold h5 mb-4">
@@ -131,13 +164,34 @@ const ProfilePage = () => {
             )}
           </Container>
           
+          
+          <Container>
+            {username ? (
+              <Card className="border border-primary p-4">
+                <Card.Title className="text-primary fw-bold h5 mb-4">
+                  Coding Status
+                </Card.Title>
+                <ul className="list-unstyled">
+                <Accordion items={accordionItems} />
+                </ul>
+
+              </Card>
+              
+
+
+            ) : (
+              <p className="text-danger">No profile data available.</p>
+            )}
+          </Container>
+          </div>
+          
         )}
       </Card.Body>
     </Card>
-
+    
     
 
-    </div>
+   
     
   );
 };
