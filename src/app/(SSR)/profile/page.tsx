4e782bf55af0589c85f11a3ac6c149f6458e467c
unknown <agarwalpratham1812@gmail.com>
2023-07-14 12:58:37 +0530

    Feat:Profile

--- src/app/(SSR)/profile/page.tsx
+++ src/app/(SSR)/profile/page.tsx
@@ -1,5 +1,7 @@
+"use client"
+
 import { useEffect, useState } from 'react';
-import { Card, Button, Spinner } from 'react-bootstrap';
+import { Card, Button, Spinner, Container } from 'react-bootstrap';
 import { useCookies } from 'react-cookie';
 import { toast } from 'react-hot-toast';
 
@@ -12,38 +14,61 @@ interface UserData {
 
 const ProfilePage = () => {
   const [cookies] = useCookies(['token']);
-  const [userData, setUserData] = useState<UserData | null>(null);
+  const [username, setUserName] = useState(null);
+  const [email, setUserEmail] = useState(null);
+  const [fname, setUserFname] = useState(null);
+  const [lname, setUserLname] = useState(null);
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    fetchProfileData();
-  }, []);
-
-  const fetchProfileData = async () => {
-    const endpoint = 'http://127.0.0.1:8000/api/auth/profile';
-    const options = {
+    async function fetchdata(){
+      
+      const endpoint = 'http://127.0.0.1:8000/api/auth/profile';
+      const options = {
       method: 'GET',
       headers: {
         Authorization: 'Bearer ' + cookies['token'],
-      },
-    };
+      }
+    }
 
-    try {
       const response = await fetch(endpoint, options);
       const data = await response.json();
-
-      if (response.ok) {
-        setUserData(data);
-      } else {
-        toast.error('Error fetching profile data');
+      const obj = JSON.parse(data)
+   
+      setUserName(obj.username)
+      setUserEmail(obj.email)
+      setUserLname(obj.lname)
+      setUserFname(obj.fname)
+      setLoading(false)
       }
-    } catch (error) {
-      console.error('Error fetching profile data:', error);
-      toast.error('Error fetching profile data');
-    } finally {
-      setLoading(false);
-    }
-  };
+  fetchdata()
+ 
+}, []);
+
+  // const fetchProfileData = async () => {
+  //   const endpoint = 'http://127.0.0.1:8000/api/auth/profile';
+  //   const options = {
+  //     method: 'GET',
+  //     headers: {
+  //       Authorization: 'Bearer ' + cookies['token'],
+  //     },
+  //   };
+
+  //   try {
+      
+
+  //     if (response.ok) {
+  //       setUserData(data);
+  //     } else {
+  //       toast.error('Error fetching profile data');
+  //     }
+  //   } catch (error) {
+  //     console.error('Error fetching profile data:', error);
+  //     toast.error('Error fetching profile data');
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
 
   return (
     <Card className="m-4 p-4 bg-light border border-primary rounded">
@@ -51,21 +76,21 @@ const ProfilePage = () => {
         <Card.Title className="mb-4 text-primary fw-bold display-4">
           User Profile
         </Card.Title>
-        <Button
+        {/* <Button
           onClick={fetchProfileData}
           variant="primary"
           className="mb-3"
           disabled={loading}
-        >
-          {loading ? (
+        ></Button> */}
+          {/* {loading ? (
             <>
               <Spinner animation="border" size="sm" className="me-2" />
               Loading...
             </>
           ) : (
             'Fetch Profile Data'
-          )}
-        </Button>
+          )} */}
+        
 
         {loading ? (
           <div className="d-flex justify-content-center my-4">
@@ -74,8 +99,8 @@ const ProfilePage = () => {
             </Spinner>
           </div>
         ) : (
-          <>
-            {userData ? (
+          <Container>
+            {username ? (
               <Card className="border border-primary p-4">
                 <Card.Title className="text-primary fw-bold h5 mb-4">
                   Profile Information
@@ -83,26 +108,23 @@ const ProfilePage = () => {
                 <ul className="list-unstyled">
                   <li className="mb-3">
                     <strong className="text-primary">First Name:</strong>{' '}
-                    {userData.fname}
+                    {fname}
                   </li>
                   <li className="mb-3">
                     <strong className="text-primary">Last Name:</strong>{' '}
-                    {userData.lname}
+                    {lname}
                   </li>
                   <li className="mb-3">
                     <strong className="text-primary">Email:</strong>{' '}
-                    {userData.email}
+                    {email}
                   </li>
-                  <li className="mb-3">
-                    <strong className="text-primary">User ID:</strong>{' '}
-                    {userData.userId}
-                  </li>
+                  
                 </ul>
               </Card>
             ) : (
               <p className="text-danger">No profile data available.</p>
             )}
-          </>
+          </Container>
         )}
       </Card.Body>
     </Card>
