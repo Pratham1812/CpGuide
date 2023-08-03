bafd45abea754fa5f6e280f2b242cc15bdaaeed9
Aaditya <aadityagupta400@gmail.com>
2023-08-03 12:24:18 +0530

    feat: updates the fields in question data DB

--- backend/db/models.py
+++ backend/db/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 class QuestionData(models.Model):
-    question_id = models.CharField(max_length=255)
-    question = models.TextField()
+    topic_id = models.CharField(max_length=3)
+    heading = models.CharField(max_length=255)
+    sub_heading = models.CharField(max_length=255)
+    topic = models.CharField(max_length=255)
+    link = models.TextField()
+   
 
     
